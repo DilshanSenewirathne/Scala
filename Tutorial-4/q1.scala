4514168d6551848b24300364cb5577757e3a4691
Dilshan Senewirathne <92708930+DilshanSenewirathne@users.noreply.github.com>
2022-08-02 01:00:45 +0530

    Update q1.scala

--- Tutorial-4/q1.scala
+++ Tutorial-4/q1.scala
@@ -1,12 +1,12 @@
 object question1 extends App{
     
     def interest(amount:Int):Double= amount match{
-        case amount if amount < 20000 => amount*.02
-        case amount if amount < 200000 => amount*.04
-        case amount if amount < 2000000 => amount*.035
-        case _=> amount*.65
+        case amount if amount <= 20000   => amount * .02
+        case amount if amount <= 200000  => amount * .04
+        case amount if amount <= 2000000 => amount * .035
+        case amount if amount >  2000000 => amount * .065
 
     }
 
-    println(interest(100000000))
+    println(interest(20000000))
 }
